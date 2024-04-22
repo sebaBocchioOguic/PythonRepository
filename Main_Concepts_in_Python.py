84b25c5f242229a71e6a3da17251e19c28c9a0e1
Sebastian Bocchio Oguic <60882728+sebaBocchioOguic@users.noreply.github.com>
2024-04-22 11:43:49 +0200

    Add DataType Comment

--- Main_Concepts_in_Python.py
+++ Main_Concepts_in_Python.py
@@ -102,3 +102,12 @@ type("Hello")
 
 # Returns the documentation fo a function
 help(print)
+
+""" 
+There are four collection data types in the Python programming language:
+
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered and changeable. No duplicate members.
+"""
